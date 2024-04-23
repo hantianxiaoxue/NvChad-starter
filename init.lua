cc90f40803988c24fae127215ea5cc8c851bbef4
webx <zh@webx.xin>
2024-04-23 22:14:55 +0800

    -- easymotion lsp bugfix

--- init.lua
+++ init.lua
@@ -15,7 +15,7 @@ local lazy_config = require "configs.lazy"
 -- load plugins
 require("lazy").setup({
   {
-    dir = vim.fn.stdpath('config').."/NvChad",
+    dir = vim.fn.stdpath "config" .. "/NvChad",
     -- "https://github.com/hantianxiaoxue/NvChad.git",
     -- branch = "v2.5",
     lazy = false,
@@ -54,6 +54,39 @@ vim.api.nvim_create_autocmd({ "InsertEnter" }, {
   end,
 })
 
+-- easymotion lsp bugfix
+vim.api.nvim_create_autocmd("User", {
+  pattern = { "EasyMotionPromptBegin" },
+  callback = function()
+    vim.diagnostic.disable()
+  end,
+})
+function check_easymotion()
+  local timer = vim.loop.new_timer()
+  timer:start(
+    500,
+    0,
+    vim.schedule_wrap(function()
+      -- vim.notify("check_easymotion")
+      if vim.fn["EasyMotion#is_active"]() == 0 then
+        vim.diagnostic.enable()
+        vim.g.waiting_for_easy_motion = false
+      else
+        check_easymotion()
+      end
+    end)
+  )
+end
+vim.api.nvim_create_autocmd("User", {
+  pattern = "EasyMotionPromptEnd",
+  callback = function()
+    if vim.g.waiting_for_easy_motion then
+      return
+    end
+    vim.g.waiting_for_easy_motion = true
+    check_easymotion()
+  end,
+})
 
 vim.schedule(function()
   require "mappings"

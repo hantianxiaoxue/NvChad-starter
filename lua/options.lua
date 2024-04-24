383b369e05c8958dd048d0d493ba3cb9c8787518
webx <zh@webx.xin>
2024-04-24 12:31:16 +0800

    term

--- lua/options.lua
+++ lua/options.lua
@@ -5,15 +5,17 @@ require "nvchad.options"
 local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
 
--- change term to pwsh
-o.shell = "pwsh.exe"
-o.shellcmdflag = "-command "
-o.shellquote = '"'
-o.shellxquote = ""
-o.shellpipe = "| Out-File -Encoding UTF8 %s"
-o.shellredir = "| Out-File -Encoding UTF8 %s"
+if vim.fn.has "win32" ~= 0 then
+  -- change term to pwsh
+  o.shell = "pwsh.exe"
+  o.shellcmdflag = "-command "
+  o.shellquote = '"'
+  o.shellxquote = ""
+  o.shellpipe = "| Out-File -Encoding UTF8 %s"
+  o.shellredir = "| Out-File -Encoding UTF8 %s"
 
--- title 在powershell使用时要在profile中添加$env:TERM = "xterm-256color"
-vim.opt.title = true
-vim.opt.titlelen = 20 -- do not shorten title
-vim.opt.titlestring = '%{expand("%:t")}'
+  -- title 在powershell使用时要在profile中添加$env:TERM = "xterm-256color"
+  vim.opt.title = true
+  vim.opt.titlelen = 20 -- do not shorten title
+  vim.opt.titlestring = '%{expand("%:t")}'
+end

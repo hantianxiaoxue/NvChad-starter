61c5b6f4984d523c8125d624c79e5864aa2e50b9
root <root@ZHOUHUI>
2024-04-24 13:16:18 +0800

    title

--- lua/options.lua
+++ lua/options.lua
@@ -13,9 +13,8 @@ if vim.fn.has "win32" ~= 0 then
   o.shellxquote = ""
   o.shellpipe = "| Out-File -Encoding UTF8 %s"
   o.shellredir = "| Out-File -Encoding UTF8 %s"
-
-  -- title 在powershell使用时要在profile中添加$env:TERM = "xterm-256color"
-  vim.opt.title = true
-  vim.opt.titlelen = 20 -- do not shorten title
-  vim.opt.titlestring = '%{expand("%:t")}'
 end
+-- title 在powershell使用时要在profile中添加$env:TERM = "xterm-256color"
+vim.opt.title = true
+vim.opt.titlelen = 20 -- do not shorten title
+vim.opt.titlestring = '%{expand("%:t")}'

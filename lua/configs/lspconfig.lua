8421e63b4a12044c308d138de1426a077afe9c8d
webx <zh@webx.xin>
2024-04-29 18:34:42 +0800

    add clangd

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "tsserver", "volar", "rust_analyzer", "vimls" ,"slint_lsp"}
+local servers = { "html", "cssls", "tsserver", "volar", "rust_analyzer", "vimls", "slint_lsp", "clangd" }
 
 local npm_home = os.getenv "NPM_HOME" or ""
 -- lsps with default config

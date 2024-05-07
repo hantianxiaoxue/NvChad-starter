88399488204d2feeda398a9229c9d62d2d714624
webx <zh@webx.xin>
2024-05-07 08:26:46 +0800

    spider

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -11,6 +11,15 @@ return {
     event = "VeryLazy",
     opts = {},
   },
+  {
+    "chrisgrieser/nvim-spider",
+    lazy = true,
+    init = function()
+      vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
+      vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-e" })
+      vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-b" })
+    end,
+  },
   { "slint-ui/vim-slint", lazy = false },
   {
     "phaazon/hop.nvim",

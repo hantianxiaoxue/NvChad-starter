41dc0316011e794c35eda163ab5b67c92af5af4e
webx <zh@webx.xin>
2024-05-08 16:36:06 +0800

    vscode-neovim conf

--- vscode.lua
+++ vscode.lua
@@ -83,6 +83,20 @@ require("lazy").setup({
       }
     end,
   },
+  {
+    "numToStr/Comment.nvim",
+    keys = {
+      { "gcc", mode = "n", desc = "Comment toggle current line" },
+      { "gc", mode = { "n", "o" }, desc = "Comment toggle linewise" },
+      { "gc", mode = "x", desc = "Comment toggle linewise (visual)" },
+      { "gbc", mode = "n", desc = "Comment toggle current block" },
+      { "gb", mode = { "n", "o" }, desc = "Comment toggle blockwise" },
+      { "gb", mode = "x", desc = "Comment toggle blockwise (visual)" },
+    },
+    config = function(_, opts)
+      require("Comment").setup(opts)
+    end,
+  },
 }, lazy_config)
 -- keymap
 

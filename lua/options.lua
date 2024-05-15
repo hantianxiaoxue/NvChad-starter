4c289364f8ae0e97667de0bdf6d759adda19af31
webx <zh@webx.xin>
2024-05-15 23:38:23 +0800

    zsh

--- lua/options.lua
+++ lua/options.lua
@@ -5,18 +5,19 @@ require "nvchad.options"
 local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
 
-if vim.fn.has "win32" ~= 0 then
-  local function is_command_exists(cmd)
-    local status = os.execute(cmd .. " --version >NUL 2>&1")
-    return status == 0
+if vim.fn.has "win32" == 1 then
+  -- change term
+  local pwsh_or_cmd = vim.fn.executable "pwsh.exe" == 1 and "pwsh.exe" or "cmd.exe"
+  o.shell = vim.fn.executable "zsh.exe" == 1 and "zsh.exe" or pwsh_or_cmd
+  if o.shell == "pwsh.exe" then
+    o.shellcmdflag = "-command "
+    o.shellquote = '"'
+    o.shellxquote = ""
+    o.shellpipe = "| Out-File -Encoding UTF8 %s"
+    o.shellredir = "| Out-File -Encoding UTF8 %s"
+  elseif o.shell == "zsh.exe" then
+    o.shellcmdflag = "-c"
   end
-  -- change term to pwsh
-  o.shell = is_command_exists "pwsh.exe" and "pwsh.exe" or "cmd.exe"
-  o.shellcmdflag = "-command "
-  o.shellquote = '"'
-  o.shellxquote = ""
-  o.shellpipe = "| Out-File -Encoding UTF8 %s"
-  o.shellredir = "| Out-File -Encoding UTF8 %s"
 end
 -- title 在powershell使用时要在profile中添加$env:TERM = "xterm-256color"
 vim.opt.title = true
